4d7a66232b493a18b6bc4be80f1d16388d273abb
John Logan <john_logan@apple.com>
2026-04-05 21:44:06 -0700

    Remove XPC test dependency for now.

--- Package.swift
+++ Package.swift
@@ -284,13 +284,6 @@ let package = Package(
                 "ContainerNetworkService",
             ]
         ),
-        .testTarget(
-            name: "ContainerXPCTests",
-            dependencies: [
-                .product(name: "Logging", package: "swift-log"),
-                "ContainerXPC",
-            ]
-        ),
         .target(
             name: "ContainerNetworkServiceClient",
             dependencies: [
